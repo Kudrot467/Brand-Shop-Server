73d23ee2805a6daa2f63afe2a41dc68fc304c9fc
Kudrot467 <kkudrotekhoda@gmail.com>
2023-10-18 23:57:37 +0600

    Brands json file added"

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const brands = require('./brands.json');
  const app=express();
 const port=process.env.PORT || 5000;
 
@@ -10,6 +11,17 @@ app.use(express.json());
 app.get('/',(req,res)=>
 {
   res.send('Brand-Server ready');
+})
+
+app.get('/brands',(req,res)=>{
+    res.send(brands)
+})
+
+app.get('/brands/:id',(req,res)=>
+{
+    const id=parseInt(req.params.id);
+    const brand=brands.find(brand=>brand.id===id) || {};
+    res.send(brand);
 })
 
 app.listen(port,()=>{
